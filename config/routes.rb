247db598e6e37dc256d274e49a2a1c0c9b2af21c
rflores <ricardo.flores@penalolen.cl>
2024-12-27 10:40:26 -0300

    devise funcionando

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  resources :posts
+  get "home/index"
+  # devise_for :users
+  devise_for :users, controllers: {
+   sessions: 'users/sessions',
+   registrations: 'users/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,5 +17,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "posts#index"
 end
